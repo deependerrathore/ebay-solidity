cb45c3d6aa52bbfbe4af904382031ce92ba628f2
Deepender Singh <deepenrathore28@gmail.com>
2024-07-07 17:26:33 +0200

    unit test: create auction

--- test/ebayTest.js
+++ test/ebayTest.js
@@ -1,8 +1,31 @@
 const Ebay = artifacts.require("Ebay");
 const { expectRevert } = require("@openzeppelin/test-helpers");
 
-contract("test", () => {
-  it("check what's in the accounts ", async () => {
-    console.log("Deep");
+contract("Ebay", (accounts) => {
+  let ebay;
+
+  beforeEach(async () => {
+    ebay = await Ebay.new();
+  });
+
+  const auction = {
+    name: "auction 1",
+    description: "Selling item 1",
+    min: 10,
+  };
+
+  const [seller, buyer1, buyer2] = [accounts[0], accounts[1], accounts[2]];
+
+  it("should create an auction", async () => {
+    let auctions;
+
+    await ebay.createAuction(auction.name, auction.description, auction.min);
+
+    auctions = await ebay.getAuctions();
+
+    assert(auctions.length === 1);
+    assert(auctions[0].name === auction.name);
+    assert(auctions[0].description === auction.description);
+    assert(Number(auctions[0].min) === auction.min);
   });
 });
